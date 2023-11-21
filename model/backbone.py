939f6878fd680dcb3edf37e19eb3ee14c0f61390
SJun99 <97303548+SJun99@users.noreply.github.com>
2023-11-21 12:42:03 +0900

    Add files via upload

--- model/backbone.py
+++ model/backbone.py
@@ -5,10 +5,12 @@ class ResNet50(nn.Module):
     def __init__(self, data_input_channel):
         super().__init__()
 
+        padding = (3 - 1) // 2
+
         self.conv_layer1 = nn.Conv2d(in_channels=data_input_channel, out_channels=16, kernel_size=3, stride=1,
-                                     padding='same')
-        self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding='same')
-        self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding='same')
+                                     padding=padding)
+        self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=padding)
+        self.conv_layer3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=padding)
 
     def forward(self, x):
         x = self.conv_layer1(x)
