e180f3bf6253ca82ffa822e94a3ce9061d9c30cd
SJun99 <97303548+SJun99@users.noreply.github.com>
2023-11-21 12:42:37 +0900

    Add files via upload

--- train/train.py
+++ train/train.py
@@ -5,13 +5,45 @@ import numpy as np
 from data.get_dataloader import get_dataloader
 from model.get_model import get_model
 from model.get_model import test_model
-
+from model.get_loss import get_loss
+from model.get_optimizer import get_optimizer
+from torch.utils.tensorboard import SummaryWriter
 
 def train_main():
-    dataloader = get_dataloader(cfg.DATA, 'train')
+    dataloader = get_dataloader(cfg.DATA)
     model = get_model(cfg.MODEL)
     loss = get_loss(cfg.LOSS)
-    optimizer = get_optimizer(cfg.OPTIMIZER)
+    optimizer = get_optimizer(cfg.OPTIMIZER, model.parameters())
+
+    # # 텐서보드를 사용하여 손실값 로깅
+    # writer = SummaryWriter()
+    #
+    # for epoch in range(cfg.TRAIN['epochs']):
+    #     # 훈련 데이터로더로부터 미니배치 가져오기
+    #     for batch_idx, batch in enumerate(dataloader):
+    #         # 입력 데이터와 레이블 추출
+    #         inputs = batch['img']
+    #         labels = batch['label']
+    #
+    #         # 모델 출력 계산
+    #         outputs = model(inputs)
+    #
+    #         # 손실 계산
+    #         loss = loss(outputs, labels)
+    #
+    #         # 옵티마이저 업데이트
+    #         optimizer.zero_grad()
+    #         loss.backward()
+    #         optimizer.step()
+    #
+    #         # 각 미니배치에서 손실값 출력
+    #         print(
+    #             f'Epoch {epoch + 1}/{cfg.TRAIN["epochs"]}, Batch {batch_idx + 1}/{len(dataloader)}, Loss: {loss.item()}')
+    #
+    #         # 텐서보드에 손실값 기록
+    #         writer.add_scalar('Training Loss', loss.item(), epoch * len(dataloader) + batch_idx)
+
+
 
     trainer = Trainer(model, loss, optimizer, cfg.TRAIN)
     trainer.train(dataloader)
@@ -19,18 +51,6 @@ def train_main():
     dataloader = get_dataloader(cfg.DATA, 'validation')
     validater = Validater(model)
     validater.eval(dataloader)
-# ResNet 직접 구현 해보기
-
-def get_loss(cfg):
-    if cfg['type'] == 'CrossEntropy':
-        loss = torch.cross_entropy()
-
-    return loss
-
-
-def get_optimizer():
-    pass
-
 
 def main():
     train_main()
