d42d542247cdeadac9f0951c7b49c61ea4047c37
SJun99 <97303548+SJun99@users.noreply.github.com>
2023-12-15 10:41:59 +0900

    Add files via upload

--- data/cifar10.py
+++ data/cifar10.py
@@ -6,20 +6,22 @@ import torch
 from torch.utils.data import Dataset
 
 
+
 class Cifar10(Dataset):
     def __init__(self, cfg, transform=None, target_transform=None):
         self.mode = cfg['dataset']['mode']
         self.path = cfg['dataset']['path']
+        self.class_info = cfg['class']['Cifar10']
 
         if self.mode == 'train':
             self.data_img_folder = os.path.join(self.path, 'train')
             self.data_label_file = os.path.join(self.path, 'trainLabels.csv')
-            self.data_label = pd.read_csv(self.data_label_file, names=['id', 'label'])
+            self.data_label = pd.read_csv(self.data_label_file, names=['id', 'label'], skiprows=1)
 
         elif self.mode == 'test':
             self.data_img_folder = os.path.join(self.path, 'train')
             self.data_label_file = os.path.join(self.path, 'sampleSubmission.csv')
-            self.data_label = pd.read_csv(self.data_label_file, names=['id', 'label'])
+            self.data_label = pd.read_csv(self.data_label_file, names=['id', 'label'], skiprows=1)
 
         else:
             sys.exit(0)
@@ -27,20 +29,23 @@ class Cifar10(Dataset):
         self.transform = transform
         self.target_transform = target_transform
 
+
     def __len__(self):
         return len(self.data_label)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.data_img_folder, self.data_label.iloc[idx, 0] + '.png')
+        img_path = os.path.join(self.data_img_folder, str(self.data_label.iloc[idx, 0]) + '.png')
         img = cv2.imread(img_path)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        label = self.data_label.iloc[idx, 1]
+        img = img / 255.0
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        label_index = self.data_label.iloc[idx, 1]
+        label = self.class_info.get(label_index, 'unknown')
 
         if self.transform:
             img = self.transform(img)
         if self.target_transform:
             label = self.target_transform(label)
 
-        sample = {'img' : torch.tensor(img, dtype=torch.float32), 'label' : label}
+        sample = {'img' : torch.tensor(img, dtype=torch.float32).permute(2,0,1), 'label' : label}
 
         return sample
